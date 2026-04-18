0f6ce10be40fa2d8dda6d9b5fc44928b210c4283
kevinnn58861-beep <kevinnn58861@gmail.com>
2026-04-18 19:00:32 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+const startDate = new Date("2024-04-18T13:00:00").getTime();
+
+setInterval(function() {
+    const now = new Date().getTime();
+    const distance = now - startDate;
+
+    const d = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const h = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const m = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const s = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("days").innerText = d;
+    document.getElementById("hours").innerText = h;
+    document.getElementById("minutes").innerText = m;
+    document.getElementById("seconds").innerText = s;
+}, 1000);
